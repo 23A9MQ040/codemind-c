3bdc991003c109869be96506c13abb12d4cd3222
Sai varma <147476920+23A9MQ040@users.noreply.github.com>
2023-10-28 07:16:28 +0530

    #include<stdio.h> int main() { 	int a ; 	int n; 	scanf("%d",&n); 	a = n; 	int i,j; 	for(i = 1; i <= a; i++) 	{ 		for(j = 1; j <= a; j++){
    		printf("%d ",i);
    	}
    	printf("
    ");
    	}
    	return 0;
    	
    }

---
+++ Basic_Pattern_1.3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int a ;
+	int n;
+	scanf("%d",&n);
+	a = n;
+	int i,j;
+	for(i = 1; i <= a; i++)
+	{
+		for(j = 1; j <= a; j++){
+		
+		printf("%d ",i);
+	}
+	printf("
+");
+	}
+	return 0;
+	
+}
