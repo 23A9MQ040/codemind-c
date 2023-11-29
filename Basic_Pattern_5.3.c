f35de9d4cc47a6b31ad33f57591a316daa254071
Sai varma <147476920+23A9MQ040@users.noreply.github.com>
2023-11-29 21:05:27 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     for(int i = a; i >= 1; i--)     {         for(int j = 1; j <= i;j++)         {             printf("%d",i);         }         for(int k = a;k > i;k--)         {                 printf("*");         }
            printf("
    ");
            
        }
    }

---
+++ Basic_Pattern_5.3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    for(int i = a; i >= 1; i--)
+    {
+        for(int j = 1; j <= i;j++)
+        {
+            printf("%d",i);
+        }
+        for(int k = a;k > i;k--)
+        {
+                printf("*");
+        }
+        
+        printf("
+");
+        
+    }
+}
