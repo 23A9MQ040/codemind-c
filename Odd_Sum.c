3875438e134bd19b460719b72af037299c2dd8d7
Sai varma <147476920+23A9MQ040@users.noreply.github.com>
2023-12-05 16:07:49 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int arr[a];     for(int i = 0 ; i < a; i++)     {         scanf("%d",&arr[i]);     }     int m = 0;     for(int j = 0 ; j < a; j++)     {         if(arr[j]%2==1)         {             m += arr[j];         }     }     printf("%d",m); }

---
+++ Odd_Sum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int arr[a];
+    for(int i = 0 ; i < a; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int m = 0;
+    for(int j = 0 ; j < a; j++)
+    {
+        if(arr[j]%2==1)
+        {
+            m += arr[j];
+        }
+    }
+    printf("%d",m);
+}
