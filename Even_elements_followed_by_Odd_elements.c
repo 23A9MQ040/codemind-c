ac635bfae067d7710ab0395af3ca0c7fefcaaf1e
Sai varma <147476920+23A9MQ040@users.noreply.github.com>
2023-12-06 19:21:03 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int arr[a];     for(int i = 0;i < a;i++){         scanf("%d",&arr[i]);     }     for(int j = 0 ; j < a; j++){         if(arr[j]%2==0)         printf("%d ",arr[j]);
        }
        for(int k = 0 ;k < a; k ++)
        {
            if(arr[k]%2!=0)
            {
                printf("%d ",arr[k]);
            }
        }
        
    }

---
+++ Even_elements_followed_by_Odd_elements.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int arr[a];
+    for(int i = 0;i < a;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int j = 0 ; j < a; j++){
+        if(arr[j]%2==0)
+        printf("%d ",arr[j]);
+    
+    }
+    for(int k = 0 ;k < a; k ++)
+    {
+        if(arr[k]%2!=0)
+        {
+            printf("%d ",arr[k]);
+        }
+    }
+    
+}
