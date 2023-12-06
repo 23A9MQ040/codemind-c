8fd5f44169ecc829106d377ccb072be623c30b31
Sai varma <147476920+23A9MQ040@users.noreply.github.com>
2023-12-06 16:06:58 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int arr[a];     for(int i = 0;i < a;i++)     {         scanf("%d",&arr[i]);     }     int l=1;     for(int j = 0 ; j < a;j++)     {         if(arr[j]%2!=0&&j%2==0)         {             l=0;             break;         }     }     printf("%s",l?"True":"False"); }

---
+++ Strictly_ODD.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int arr[a];
+    for(int i = 0;i < a;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int l=1;
+    for(int j = 0 ; j < a;j++)
+    {
+        if(arr[j]%2!=0&&j%2==0)
+        {
+            l=0;
+            break;
+        }
+    }
+    printf("%s",l?"True":"False");
+}
+    
